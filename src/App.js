0c16caee210bf38a7dffc632a1a030c6ff43cdc0
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2025-01-13 07:52:04 +0400

    feat: added loading delay

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useWindowSize } from "rooks";
 import styled from "styled-components";
 import PokerTable from "./components/PokerTable";
 import TableContextProvider from "./context/TableContext";
 import useScale from "./hooks/useScale";
 
+const delay = () => new Promise((resolve) => setTimeout(() => resolve(), 1000));
+
 function App() {
   const { innerWidth, innerHeight } = useWindowSize();
   const { scale } = useScale(innerWidth, innerHeight);
+  const [isLoading, setIsloading] = useState(false);
+
+  useEffect(() => {
+    delay().then(() => {
+      setIsloading(true);
+    });
+  }, []);
 
   return (
     <TableContextProvider>
-      <AppContainer scale={scale}>
-        <PokerTable />
-      </AppContainer>
+      {isLoading ? (
+        <AppContainer scale={scale}>
+          <PokerTable />
+        </AppContainer>
+      ) : <Loader>Loading...</Loader>}
     </TableContextProvider>
   );
 }
@@ -27,5 +38,11 @@ export const AppContainer = styled.div`
   justify-content: center;
   background-color: #000;
 `;
+
+const Loader = styled.div`
+  text-align: center;
+  color: white;
+  padding: 4rem 0;
+`
 
 export default App;
